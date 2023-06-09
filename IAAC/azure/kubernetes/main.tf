7eccfc729039233296061656d8c487f4e38d6893
Richard Acheampong <44090445+Richpong212@users.noreply.github.com>
2023-06-09 23:00:30 +0200

    Update main.tf

--- IAAC/azure/kubernetes/main.tf
+++ IAAC/azure/kubernetes/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "resource_group" {
     name     = "${var.resource_group}_${var.environment}"
     location = var.location
