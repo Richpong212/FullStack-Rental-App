30a9baaa54b403c3dba8bbe30cf5f9d5d2fd09b3
Richard Acheampong <44090445+Richpong212@users.noreply.github.com>
2023-06-09 12:02:39 +0200

    Update main.tf

--- IAAC/azure/kubernetes/main.tf
+++ IAAC/azure/kubernetes/main.tf
@@ -1,43 +1,40 @@
 resource "azurerm_resource_group" "resource_group" {
-    name  = "${var.resource_group}_${var.environment}"
-    location =  var.location
+    name     = "${var.resource_group}_${var.environment}"
+    location = var.location
 }
 
 resource "azurerm_kubernetes_cluster" "terraform-k8s" {
-  name = "${var.resource_group}_${var.environment}"
-  location = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  dns_prefix = var.dns_prefix
-}
+    name                = "${var.resource_group}_${var.environment}"
+    location            = azurerm_resource_group.resource_group.location
+    resource_group_name = azurerm_resource_group.resource_group.name
+    dns_prefix          = var.dns_prefix
 
-linux_profile {
-  admin_username =  "ubuntu"
-  
-  ssh_key {
-        key_data = file(var.ssh_public_key)
+    linux_profile {
+        admin_username = "ubuntu"
+
+        ssh_key {
+            key_data = file(var.ssh_public_key)
+        }
     }
-}
 
-default_node_pool {
-  name = "agentpool"
-  node_count = 1
-  vm_size = "Standard_DS1_v2"
-}
+    default_node_pool {
+        name       = "agentpool"
+        node_count = 1
+        vm_size    = "Standard_DS1_v2"
+    }
 
-service_principal {
-  client_id     = var.client_id
-  client_secret = var.client_secret
-}
+    service_principal {
+        client_id     = var.client_id
+        client_secret = var.client_secret
+    }
 
-tags = {
-    Environment = var.environment
+    tags = {
+        Environment = var.environment
+    }
 }
 
 terraform {
     backend "azurerm" {
-       # resource_group_name  = var.resource_group
-       #storage_account_name = var.storage_account
-       # container_name       = var.container_name
-       # key                  = var.key
+        # Define your backend configuration here
     }
 }
