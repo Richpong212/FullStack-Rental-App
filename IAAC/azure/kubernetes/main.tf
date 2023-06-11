c9d34c380c415d33d5c18e1dc070858266a72e7e
Richard Acheampong <44090445+Richpong212@users.noreply.github.com>
2023-06-11 08:23:51 +0200

    Update main.tf

--- IAAC/azure/kubernetes/main.tf
+++ IAAC/azure/kubernetes/main.tf
@@ -10,10 +10,6 @@ terraform {
 resource "azurerm_resource_group" "resource_group" {
     name     = "${var.resource_group}_${var.environment}"
     location = var.location
-    features = {
-        enable_all = false
-        
-    }
 }
 
 
@@ -23,13 +19,16 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     location            = azurerm_resource_group.resource_group.location
     resource_group_name = azurerm_resource_group.resource_group.name
     dns_prefix          = var.dns_prefix
-
     linux_profile {
         admin_username = "ubuntu"
 
         ssh_key {
             key_data = file(var.ssh_public_key)
         }
+    }
+
+    feeature {
+        azure_policy {}
     }
 
     default_node_pool {
