30c1fd2300ef3fda414c2c686a81f57a69ea5b42
richpong212 <richpong@hotmail.com>
2023-06-12 06:29:28 +0000

    k8s aws updated

--- IAAC/aws/kubernetes/main.tf
+++ IAAC/aws/kubernetes/main.tf
@@ -14,42 +14,46 @@ resource "aws_default_vpc" "default" {
   
 }
 
-data "aws_subnet" "subnets" {
+data "aws_subnet_ids" "subnets" {
   vpc_id = aws_default_vpc.default.id
 }
 
-resource "kubernetes_cluster_role_binding" "name" {
-    metadata {
-        name = "fabric8-rbac"
-    }
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind      = "ClusterRole"
-        name      = "cluster-admin"
-    }
-    subject {
-        kind      = "ServiceAccount"
-        name      = "default"
-        api_group = "default"
-    }
-  
+provider "kubernetes" {
+  //>>Uncomment this section once EKS is created - Start
+  # host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  # token                  = data.aws_eks_cluster_auth.cluster.token
+  //>>Uncomment this section once EKS is created - End
 }
+
 
 module "richpong-cluster" {
   source = "terraform-aws-modules/eks/aws"
   cluster_name = "richpong-cluster"
-  cluster_version = "1.14"
+  cluster_version = "1.23"
   subnets = ["subnet-0b12d33e10affbbcf","subnet-0a1df796a0109377e"]
   vpc_id = aws_default_vpc.default.id
 
-  node_groups = [
-    {
-      name                          = "richpong-cluster-node-group"
-      instance_type                 = "t2.micro"
-      max_capacity                  = 3
-      min_capacity                  = 3
-      desired_capacity              = 1
-      
+  cluster_endpoint_public_access  = true
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t2.small", "t2.medium"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t2.medium"]
     }
-  ]
+  }
+}
+
+# Needed to set the default region
+provider "aws" {
+  region  = "us-east-1"
 }
