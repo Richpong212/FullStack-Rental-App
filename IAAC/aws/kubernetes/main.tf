99d8192b621c79a63f8844554987217048068aad
richpong212 <richpong@hotmail.com>
2023-06-12 11:27:54 +0000

    updated

--- IAAC/aws/kubernetes/main.tf
+++ IAAC/aws/kubernetes/main.tf
@@ -24,7 +24,6 @@ module "richpong-cluster" {
   source            = "terraform-aws-modules/eks/aws"
   cluster_name      = "richpong-cluster"
   cluster_version   = "1.23"
-  subnets           = ["subnet-0b12d33e10affbbcf", "subnet-0a1df796a0109377e"]
   vpc_id            = aws_default_vpc.default.id
   cluster_endpoint_public_access = true
 
