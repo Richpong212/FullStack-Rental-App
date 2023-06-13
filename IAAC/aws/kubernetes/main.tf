4d6e80581d1f503a4fa8eaf16efbc9e74a2641ab
richpong212 <richpong@hotmail.com>
2023-06-13 13:19:17 +0000

    updated

--- IAAC/aws/kubernetes/main.tf
+++ IAAC/aws/kubernetes/main.tf
@@ -1,3 +1,37 @@
+# Needed to set the default region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "default-vpc"
+  }
+}
+
+data "aws_subnet" "subnets" {
+  vpc_id            = aws_default_vpc.default.id
+  availability_zone = "us-east-1a"
+}
+
+provider "kubernetes" {
+  // Uncomment this section once EKS is created
+  // host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+  // cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  // token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 module "richpong-cluster-eks" {
   source            = "terraform-aws-modules/eks/aws"
   cluster_name      = "richpong-cluster"
@@ -5,19 +39,18 @@ module "richpong-cluster-eks" {
   vpc_id            = aws_default_vpc.default.id
   cluster_endpoint_public_access = true
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    # instance_types = ["t2.small", "t2.medium"]
+    // instance_types = ["t2.small", "t2.medium"]
   }
 
   eks_managed_node_groups = {
-    # blue = {}
-    # green = {
-    #   min_size       = 1
-    #   max_size       = 10
-    #   desired_size   = 1
-    #   instance_types = ["t2.medium"]
-    # }
+    // blue = {}
+    // green = {
+    //   min_size       = 1
+    //   max_size       = 10
+    //   desired_size   = 1
+    //   instance_types = ["t2.medium"]
+    // }
   }
 
 }
