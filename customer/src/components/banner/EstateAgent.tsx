036ab8a7ad324aec43ca6fa154a1cd85318a534a
richpong212 <richpong@hotmail.com>
2023-06-16 08:11:10 +0000

    propertyDetails

--- customer/src/components/banner/EstateAgent.tsx
+++ customer/src/components/banner/EstateAgent.tsx
@@ -1,45 +1,52 @@
-import React from 'react'
-import "./banner.scss"
-import { red } from '@mui/material/colors';
-
+import React from "react";
+import "./banner.scss";
+import { red } from "@mui/material/colors";
 
 const EstateAgent = () => {
-    // Setting up background icon
-  const publicUrl = process.env.PUBLIC_URL; 
+  // Setting up background icon
+  const publicUrl = process.env.PUBLIC_URL;
 
   return (
     <div>
-    <div style={{
+      {/* Estate Agent Banner */}
+      <div
+        style={{
           backgroundColor: "#ECA760",
-         }}
-          className="estate__container ">
+        }}
+        className="estate__container "
+      >
+        {/* Title */}
+        <div
+          style={{
+            flex: "2",
+            color: "#fff",
+          }}
+          className=""
+        >
+          <h2>Become A Rental Estate Agent</h2>
+        </div>
 
-          <div style={{ flex:"2", color:"#fff", }} className="">
-               <h2>Become A Rental Estate Agent</h2>
-          </div>
-     <div style={{
-            backgroundImage: "url("+ publicUrl +" /assets/img/icons/26.png)",
+        {/* Background Icon */}
+        <div
+          style={{
+            backgroundImage: `url(${publicUrl} /assets/img/icons/26.png)`,
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center center",
             width: "100%",
             padding: "4rem 0rem",
             backgroundSize: "16rem",
             flex: "1",
-            
-          
           }}
-             className="">
-             
-        </div>
-        <div >
-             <button className="estateagent__registerbtn ">
-              Register Now
-             </button>
-        </div>
-        </div>
-        </div>
-  )
-}
+          className=""
+        ></div>
 
-export default EstateAgent
+        {/* Register Button */}
+        <div>
+          <button className="estateagent__registerbtn ">Register Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+export default EstateAgent;
--- customer/src/pages/Property/Property.tsx
+++ customer/src/pages/Property/Property.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import SinglePropertyHeader from "../../components/headers/SinglePropertyHeader/SinglePropertyHeader";
+import PropertyDetails from "../../components/PropertyDetail/PropertyDetails";
 
 const Property = () => {
-  return <div>Property</div>;
+  return (
+    <>
+      {/* Render the SinglePropertyHeader component */}
+      <SinglePropertyHeader />
+
+      {/* Render the PropertyDetails component */}
+      <PropertyDetails />
+    </>
+  );
 };
 
 export default Property;
