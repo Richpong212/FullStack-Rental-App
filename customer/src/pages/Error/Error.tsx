bb65bb96af931168784ed243fb15edad0ad2d8e9
wilfy7 <wilfy7am@gmail.com>
2023-06-21 11:09:06 +0200

    newupdate

--- customer/src/pages/Error/Error.tsx
+++ customer/src/pages/Error/Error.tsx
@@ -12,7 +12,7 @@ const Error = () => {
      < h1 className="error__h1">404
      </h1>
 
-     <p style={{color:"#343434"}}>We can't find the page you're looking for.
+     <p style={{color:"#343434"}}>We can't find the page you're looking for
      </p>
 
      <a href="/" className="container__a container__ahover"> Go back</a>
