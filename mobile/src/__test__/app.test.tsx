90e91efc2f702d1e3a4c327939abbdb20b037f95
Richpong212 <richpong123@gmail.com>
2023-07-07 15:55:02 +0200

    mobile-version

---
+++ mobile/src/__test__/app.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import App from "../../App";
+
+describe("App", () => {
+  //should render the app
+  it("should render the app", () => {});
+});
