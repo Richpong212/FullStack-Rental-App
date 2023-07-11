7404e5d044f6e55e72dda778ba6f5f9afb59e15e
wilfy7 <wilfy7am@gmail.com>
2023-07-11 22:50:44 +0200

    updating

--- customer/src/pages/AboutUs.tsx
+++ customer/src/pages/AboutUs.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 const AboutUs = () => {
+  {/* Background image for the About us page */}
+  const publicUrl=process.env.PUBLIC_URL
   return (
-    <div className="globalmainstyles">
-      About Us
+    <div className="faq__container">
+        <div 
+      style={{
+        backgroundImage:
+          "url(" + publicUrl + "/assets/img/banner/contact4.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        width: "100%",
+       height: "60vh",
+    }} className="faq__gap">
+      <h1 style={{
+        color:"#fff",
+        fontWeight: "800",
+        letterSpacing: "2px",
+        paddingTop: "22rem",
+        paddingLeft: "4rem"
+      
+         }}>About
+      </h1>  
+    </div>
     </div>
   )
 }
